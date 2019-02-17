60d8150637ecbe98bef907042a590f9c0865e54e
Michael Maurer <mjmaurer777@gmail.com>
2019-02-17 14:12:25 -0500

    add env constants 2

---
+++ constants.py
@@ -0,0 +1,7 @@
+import os
+
+GMAIL_USER = str(os.getenv('GMAIL_USER'))
+GMAIL_PASS = str(os.getenv('GMAIL_PASS'))
+GMAIL_TARGET = str(os.getenv('GMAIL_TARGET'))
+PEDALS_ID = str(os.getenv('PEDALS_ID'))
+PEDALS_SECRET = str(os.getenv('PEDALS_SECRET'))
