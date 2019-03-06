094f0bc8644d1c8ed9c78702d8b39512aa653169
Michael Maurer <mjmaurer777@gmail.com>
2019-03-05 21:24:41 -0500

    timestamp

--- pedal.py
+++ pedal.py
@@ -2,22 +2,26 @@ import praw
 import re
 import smtplib
 import constants
-import datetime
+import datetime 
 
 def main():
     reddit = praw.Reddit(user_agent='Pedals (by /u/mauflows)',
                          client_id=constants.PEDALS_ID,
                          client_secret=constants.PEDALS_SECRET)
-
+    startTime = datetime.datetime.now();
     subreddit = reddit.subreddit('PedalDeals+SynthDeals')
     for submission in subreddit.stream.submissions():
-        process_submission(submission)
+        process_submission(submission, startTime)
 
 
-def process_submission(submission):
+def process_submission(submission, startTime):
     try:
         rating = int(re.search('\\d+%$', submission.title).group(0).strip('%'))
-        if (rating > 97):
+        postTime = datetime.datetime.fromtimestamp(submission.created)
+        if (rating > 97 and postTime > startTime):
+            print('sending text')
+            print('start time: ' + startTime.strftime('%Y-%m-%d %H:%M:%S.%f %Z'))
+            print('post time: ' + postTime.strftime('%Y-%m-%d %H:%M:%S.%f %Z'))
             sendText(submission)
     except Exception as e:
         print('Bad title?: ' + str(submission.title))
